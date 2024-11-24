0c0b0ddc90ac897eba87134b9d08e21837e90571
hemisemidemipresent <hartmannsyg@gmail.com>
2024-11-24 16:02:21 +0800

    update FS pfp

--- themes/cactus/scripts/person.js
+++ themes/cactus/scripts/person.js
@@ -11,7 +11,7 @@ const people = {
         image: "https://cdn.discordapp.com/avatars/1154815737341419530/642de2cc83c049d7aa44a1ebb4b43331.png",
     },
     fs: {
-        image: "https://cdn.discordapp.com/avatars/933347118402392085/80aba864de7c265f9f11c94b02709fcf.png",
+        image: "https://cdn.discordapp.com/avatars/933347118402392085/c0af9cb752f6db7984931306a5a2d204.png",
     },
     treeindustry: {
         image: "https://cdn.discordapp.com/avatars/637581035227447296/84fe734b031efc508797991d7642d295.png",
